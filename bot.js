63a64d341181cc903fb5fc79dd0d8db225f65f5e
imraider <45240051+imraider@users.noreply.github.com>
2019-01-04 19:33:00 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -12,6 +12,20 @@ client.user.setGame(`My Prefix [-]`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
+client.on('message' , message => { 
+    var prefix = "-";
+     if (message.content === prefix + "servers") {
+
+if(!message.channel.guild) return;
+  if(message.content < 1023) return
+  const Embed11 = new Discord.RichEmbed()
+.setAuthor(client.user.username,client.user.avatarURL)
+.setThumbnail(client.user.avatarURL)
+.setDescription(`***السيرفرات الموجودة فيه البوت ${client.guilds.size} \n \n${client.guilds.map(guilds => `- ${guilds.name}`).join('\n')}***`)
+         message.channel.sendEmbed(Embed11)
+    }
+});
+
 client.on('message', message => {
 	 var prefix ="-";
  if(message.content.startsWith(prefix +"server")){
